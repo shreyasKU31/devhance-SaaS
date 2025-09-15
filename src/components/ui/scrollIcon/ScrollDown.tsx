e7ddd9f90f1f85fac0daecf776b5ecd678af04d0
shreyasKU31 <shreyasku31@gmail.com>
2025-09-15 18:14:57 +0530

    feat(UI) : Add scroll icon

---
+++ src/components/ui/scrollIcon/ScrollDown.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./scrollIcon.module.css";
+const ScrollDown = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.field}>
+        <div className={styles.mouse}></div>
+      </div>
+    </div>
+  );
+};
+
+export default ScrollDown;
