6fd7904ba19dbfe71b9c5356384615442640b3a5
shreyasKU31 <shreyasku31@gmail.com>
2025-09-19 20:56:01 +0530

    Add final cta section

--- src/components/sections/cta/CTA.tsx
+++ src/components/sections/cta/CTA.tsx
@@ -1,17 +1,28 @@
-import Buttons from "@/components/ui/Button";
-import React from "react";
+// components/CtaSection.tsx
 
-const CTA = () => {
+import type { FC } from "react";
+import styles from "./style.module.css";
+import Buttons from "@/components/ui/Button";
+
+const CTA: FC = () => {
   return (
-    <section>
-      <div>
-        <h2>Ready to Build Credibility?</h2>
-        <p>
+    <section className="relative py-20 w-[100vw] h-[500px] text-center overflow-hidden flex flex-col items-center justify-center">
+      <div className={`brand ${styles.animatedBrandBg}`}></div>
+
+      {/* Foreground Content */}
+      <div className="flex flex-col items-center gap-4">
+        <h2 className="text-4xl font-bold tracking-tight text-white md:text-6xl">
+          Ready to Build Credibility?
+        </h2>
+        <span className="max-w-4xl text-white">
           Join DevHance today and transform your work into a powerful story that
           inspires trust and showcases your momentum.
-        </p>
+        </span>
       </div>
-      <Buttons text="Become the Source of Truth" style="" />
+      <Buttons
+        text="Become the Source of Truth"
+        style="bg-black px-16 py-4 rounded-full mt-10"
+      />
     </section>
   );
 };
