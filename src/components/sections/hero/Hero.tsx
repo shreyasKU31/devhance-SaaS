4d59daa451b39deefb7066c23ba09978a786fec7
shreyasKU31 <shreyasku31@gmail.com>
2025-09-20 19:14:49 +0530

    UI fix in hero section

--- src/components/sections/hero/Hero.tsx
+++ src/components/sections/hero/Hero.tsx
@@ -4,15 +4,17 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <section className="pt-30 max-w-5xl h-[100vh] text-center px-10 flex flex-col gap-15">
-      <h1 className="pt-8">
-        Your Product Has a <span>Story.</span> <br /> <span>Tell It.</span>
-      </h1>
-      <p>
-        DevHance is the single source of truth for your build. We empower
-        founders to connect their code, designs, and decisions into one
-        compelling story that builds undeniable trust.
-      </p>
+    <section className="pt-40 h-[100vh] text-center px-10 flex flex-col gap-16 items-center">
+      <div className="flex flex-col gap-8 items-center">
+        <h1 className="pt-8">
+          Your Product has a <span>Story.</span> <br /> <span>Tell It.</span>
+        </h1>
+        <p className="max-w-4xl text-center">
+          DevHance is the single source of truth for your build. We empower
+          founders to connect their code, designs, and decisions into one
+          compelling story that builds undeniable trust.
+        </p>
+      </div>
       <div className="flex justify-center gap-8">
         <Buttons
           text="Start Your Build Log"
