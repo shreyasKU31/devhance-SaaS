a48362b0272294a2cd9a99f62be32bd79f66a697
shreyasKU31 <shreyasku31@gmail.com>
2025-09-28 15:22:44 +0530

    add sidebar

---
+++ src/components/dashboard/sidebar/SideBar.tsx
@@ -0,0 +1,78 @@
+import {
+  LayoutDashboard,
+  FileText,
+  BarChart2,
+  Share2,
+  Settings,
+} from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+// Menu items.
+const items = [
+  {
+    title: "Dashboard",
+    url: "/dashboard",
+    icon: LayoutDashboard,
+  },
+  {
+    title: "My Stories",
+    url: "/stories",
+    icon: FileText,
+  },
+  {
+    title: "Analytics",
+    url: "/analytics",
+    icon: BarChart2,
+  },
+  {
+    title: "Public View",
+    url: "/[username]", // This would need to be dynamically generated
+    icon: Share2,
+  },
+  {
+    title: "Settings",
+    url: "/settings",
+    icon: Settings,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel className="text-xl my-4">
+            Application
+          </SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem
+                  key={item.title}
+                  className="mb-2 rounded-xl hover:bg-gray-800"
+                >
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span className="text-base">{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
