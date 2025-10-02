b463034e94b6323c756446d17a3267d38970b9f0
shreyasKU31 <shreyasku31@gmail.com>
2025-10-02 18:18:08 +0530

    add interfaces

---
+++ src/app/interfaces/formData.ts
@@ -0,0 +1,9 @@
+export interface FormData {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  address: string;
+  city: string;
+  country: string;
+}
---
+++ src/app/interfaces/steps.ts
@@ -0,0 +1,6 @@
+import { ReactNode } from "react";
+
+export interface Step {
+  title: string;
+  component: ReactNode;
+}
