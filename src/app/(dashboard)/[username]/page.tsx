e557aaef8d35e259a83255040c9fb047227bd181
shreyasKU31 <shreyasku31@gmail.com>
2025-10-02 08:16:13 +0530

    fix error handling

--- src/app/(dashboard)/[username]/page.tsx
+++ src/app/(dashboard)/[username]/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const page = async ({ params }: { params: { username: string } }) => {
   const { username } = await params;
-  return <div>{username}</div>;
+  return <div>{username || "User"}</div>;
 };
 
 export default page;
